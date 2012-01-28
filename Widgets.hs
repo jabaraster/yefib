9442ec49fa53cf1bcf43582c9bdb792750cb1a6d
jabaraster <jabaraster@gmail.com>
2012-01-28 11:19:37 +0900

    master

--- Widgets.hs
+++ Widgets.hs
@@ -1,8 +1,23 @@
 module Widgets where
 
 import Import
-import Data.Text
 
 submitWidget :: GGWidget Jabaraster (GHandler Jabaraster Jabaraster) ()
 submitWidget = do
+  addScriptRemote "https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"
   $(widgetFile "submit")
+
+submitWidget' :: Text -> GGWidget Jabaraster (GHandler Jabaraster Jabaraster) ()
+submitWidget' tagId = do
+  addScriptRemote "https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"
+  $(widgetFile "submit2")
+
+data SubmitWidgetContext = SubmitWidgetContext {
+  submitTagId :: Text
+  , submitOther :: Text
+}
+
+submitWidget'' :: SubmitWidgetContext -> GGWidget Jabaraster (GHandler Jabaraster Jabaraster) ()
+submitWidget'' context = do
+  addScriptRemote "https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"
+  $(widgetFile "submit3")
